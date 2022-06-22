feef689659ee566357212823c085ac4352a73c3b
stidsborg <stidsborg@cleipnir.net>
2022-06-22 13:04:13 +0200

    Simplified ProductsClient to running multiple instances concurrently

--- Order.WebApi/Communication/ProductsClient.cs
+++ Order.WebApi/Communication/ProductsClient.cs
@@ -1,4 +1,5 @@
-﻿using Order.WebApi.Domain;
+﻿using Cleipnir.ResilientFunctions.Helpers;
+using Order.WebApi.Domain;
 
 namespace Order.WebApi.Communication;
 
@@ -9,24 +10,6 @@ public interface IProductsClient
 
 public class ProductsClientStub : IProductsClient
 {
-    private readonly Dictionary<Guid, decimal> _prices = new();
-    private readonly object _sync = new();
-    private readonly Random _random = new();
-
-    public Task<IEnumerable<ProductPrice>> GetProductPrices(IEnumerable<Guid> productIds)
-    {
-        lock (_sync)
-        {
-            var toReturn = new List<ProductPrice>();
-            foreach (var productId in productIds)
-            {
-                if (!_prices.ContainsKey(productId)) 
-                    _prices[productId] = _random.Next(1, 100);    
-
-                toReturn.Add(new ProductPrice(productId, _prices[productId]));
-            }
-
-            return Task.FromResult((IEnumerable<ProductPrice>) toReturn);
-        }
-    }
+    public Task<IEnumerable<ProductPrice>> GetProductPrices(IEnumerable<Guid> productIds) 
+        => productIds.Select(id => new ProductPrice(id, 100M)).ToTask();
 }
