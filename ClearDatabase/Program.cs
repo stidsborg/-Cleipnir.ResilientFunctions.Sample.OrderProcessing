50bfe91fdacfaa151c0d9072f0ecd39553bc3345
stidsborg <stidsborg@cleipnir.net>
2022-06-29 09:08:47 +0200

    Added ClearDatabase cli project

---
+++ ClearDatabase/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Npgsql;
+
+const string connectionString = "Server=localhost;Port=5432;Userid=postgres;Password=Pa55word!;Database=presentation;";
+var conn = new NpgsqlConnection(connectionString);
+conn.Open();
+var command = new NpgsqlCommand("TRUNCATE TABLE orders; TRUNCATE TABLE rfunctions;", conn);
+command.ExecuteNonQuery();
+Console.WriteLine("Tables truncated successfully");
